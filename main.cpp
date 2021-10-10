e85544b08531e348f4a335b1b03ab5b2b2805599
molok0 <molok0@protonmail.com>
2021-10-10 14:18:05 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF 100000007
+
+typedef long long ll;
+//typedef vector<int> vi;
+//typedef vector<ll> vll;
+//typedef pair<int, int> pii;
+
+inline void speed()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    speed();
+
+    return 0;
+}
